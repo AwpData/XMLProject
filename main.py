00aaaee2a0108e727bcbcf73b4272d002c53750f
Trevor Tang <awpdata@gmail.com>
2021-08-26 16:44:49 -0700

    Final project uploaded

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 import re
 import string
-from collections import Counter
 
 import nltk
+import pandas as pd
 from lxml import etree
 from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 nltk.download("stopwords")
 nltk.download("wordnet")
+nltk.download("averaged_perceptron_tagger")
 
-stopwords_ = stopwords.words("english")
-punctuation = list(string.punctuation)
 lemma = WordNetLemmatizer()
+punctuation = list(string.punctuation)
+stopwords_ = stopwords.words("english")
+
 root = etree.parse("news.xml").getroot()[0]
-for i in root:
-    title = i[0].text
+stories = list()
+
+for i in root:  # First, we must reduce, remove punctuation / stopwords, and only pick the nouns before TF-IDF
     story = i[1].text
     word_tokenizer = nltk.word_tokenize(story.lower())
-    word_tokenizer.sort(reverse=True)
     word_tokenizer = [lemma.lemmatize(word) for word in word_tokenizer]
     word_tokenizer = [word for word in word_tokenizer if word not in punctuation]
     word_tokenizer = [word for word in word_tokenizer if word not in stopwords_]
-    word_tokenizer = [word for word in word_tokenizer if re.match(r"'[\w\d]+", word) is None]  # Make sure to de comment
-    common_word_dict = Counter(word_tokenizer)
-    print(title + ":")
-    for word in common_word_dict.most_common(5):
-        print(word[0], end=" ")
+    word_tokenizer = [word for word in word_tokenizer if nltk.pos_tag([word])[0][1] == "NN"]
+    word_tokenizer = [word for word in word_tokenizer if re.match(r"'[\w\d]+", word) is None]
+    stories.append(" ".join(word_tokenizer))
+
+vectorizer = TfidfVectorizer()
+matrix = vectorizer.fit_transform(stories).toarray()
+terms = vectorizer.get_feature_names()  # The vocab of all the stories
+
+current_story = 0  # This will be used to loop through the score matrix / title of each news story
+for i in root:
+    df = pd.DataFrame()
+    df["Words"] = terms
+    df["Score"] = matrix[current_story]  # Score is based off of our array of TF-IDF scores
+    df.sort_values(["Score", "Words"], ascending=False, inplace=True)  # Sorts the words by TF-IDF score
+    data = df.iloc[0:5]["Words"]  # Gets the 5 most common words into a series (list)
+    print("{}:".format(i[0].text))  # title
+    for word in data:
+        print(word, end=" ")  # 5 most common words in dataframe's word series
     print("\n")
+    current_story += 1
